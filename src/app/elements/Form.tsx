907d17785610db76d535771538cdb541eadc8ea1
nivindulakshitha <nivindulakshitha@gmail.com>
2024-10-08 08:11:29 +0530

    Refactor Form component to include loading state and handle form submission

--- src/app/elements/Form.tsx
+++ src/app/elements/Form.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 
 const Form = () => {
-  return (
-      <div className='form-element'>
-      <h2 className="head-text">URL Shortener</h2>
-      <input className='url-input' type="url" name="long-url" id="long-url" required placeholder='Enter a long URL to shorten' />
-    </div>
-  )
+	const [loading, setLoading] = useState(false);
+	const handleInputLongUrl = () => {
+		console.log("input long url")
+	}
+
+	const handleSubmit = () => {
+		setLoading(true)
+		console.log("submit")
+	}
+	return (
+		<div className='form-element'>
+			<h2 className="head-text">URL Shortener</h2>
+			<input className='url-input' onInput={handleInputLongUrl} type="url" name="long-url" id="long-url" required placeholder='Enter a long URL to shorten' />
+			<button onClick={handleSubmit} disabled={loading}>
+				{ loading? "Shortening..." : "Shorten URL" }
+			</button>
+		</div>
+	)
 }
 
 export default Form
