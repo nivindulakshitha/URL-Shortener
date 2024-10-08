2ee70744734767655aff9bb0bc144da7596214b2
nivindulakshitha <nivindulakshitha@gmail.com>
2024-10-08 07:50:58 +0530

    Refactor CSS and remove Input component

--- src/app/elements/Form.tsx
+++ src/app/elements/Form.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Form = () => {
   return (
       <div className='form-element'>
-          <h2 className="head-text">URL Shortener</h2>
+      <h2 className="head-text">URL Shortener</h2>
+      <input className='url-input' type="url" name="long-url" id="long-url" required placeholder='Enter a long URL to shorten' />
     </div>
   )
 }
