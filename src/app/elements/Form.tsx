95a75092970dc2a01217fc2ee83323a686e54f31
nivindulakshitha <nivindulakshitha@gmail.com>
2024-10-08 19:07:55 +0530

    Refactor Form component to include copyable URL input and update styling

--- src/app/elements/Form.tsx
+++ src/app/elements/Form.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Check, Copy, LoaderCircle } from 'lucide-react';
 import React, { useRef, useState } from 'react'
 
 const Form = () => {
@@ -31,17 +32,16 @@ const Form = () => {
 		<div className='form-element'>
 			<h2 className="head-text">URL Shortener</h2>
 			<input className='url-input' onInput={handleInputLongUrl} type="url" name="long-url" id="long-url" required placeholder='Enter a long URL to shorten' />
-			<button onClick={handleSubmit} disabled={loading}>
-				{ loading? "Shortening..." : "Shorten URL" }
+			<button type='submit' onClick={handleSubmit} disabled={loading}>
+				{ loading ? "Shortening..." : "Shorten URL" }
+				{ loading? <LoaderCircle className="animate-spin" /> : null }
 			</button>
 			<span>Example: https://www.goole.com</span>
 			<span>Shortened URL</span>
 			<div className="copyble-area">
 				<input className='url-input' ref={shortUrlRef} onInput={handleInputLongUrl} type="url" name="short-url" id="short-url" required placeholder='Your URL shortened' disabled />
-				{
-					!copying ? <svg onClick={handleCopy} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z" /></svg> : <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#00FF00"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" /></svg>
-
-				}
+				{ 
+					!copying ? <Copy onClick={handleCopy}/> : <Check className='text-green-600 !opacity-70'/>				}
 			</div>
 		</div>
 	)
