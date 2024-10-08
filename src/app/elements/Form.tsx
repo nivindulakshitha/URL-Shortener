e8384602839f96d6131f93fcefa4c397c88e30bc
nivindulakshitha <nivindulakshitha@gmail.com>
2024-10-08 08:55:52 +0530

    Refactor Form component to disable copyable URL input and add icons

--- src/app/elements/Form.tsx
+++ src/app/elements/Form.tsx
@@ -1,7 +1,8 @@
 "use client";
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 const Form = () => {
+	const shortUrlRef = useRef();
 	const [loading, setLoading] = useState(false);
 	const handleInputLongUrl = () => {
 		console.log("input long url")
@@ -10,6 +11,10 @@ const Form = () => {
 	const handleSubmit = () => {
 		setLoading(true)
 		console.log("submit")
+	}
+
+	const handleCopy = () => { 
+
 	}
 	return (
 		<div className='form-element'>
@@ -21,7 +26,9 @@ const Form = () => {
 			<span>Example: https://www.goole.com</span>
 			<span>Shortened URL</span>
 			<div className="copyble-area">
-				<input className='url-input' onInput={handleInputLongUrl} type="url" name="short-url" id="short-url" required placeholder='Your URL shortened' />
+				<input className='url-input' onInput={handleInputLongUrl} type="url" name="short-url" id="short-url" required placeholder='Your URL shortened' disabled />
+				<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z" /></svg>
+				<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" /></svg>
 			</div>
 		</div>
 	)
