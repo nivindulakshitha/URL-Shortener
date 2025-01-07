76a6c2a1a712eeeb79a1a799d4fd4a528cce5b3a
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-01-07 14:07:07 +0530

    Refactor NotFoundNote component to use Suspense for loading state and improve code organization

--- src/app/components/NotFoundNote.tsx
+++ src/app/components/NotFoundNote.tsx
@@ -2,23 +2,31 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
-import React from 'react'
+import React, { Suspense } from 'react'
 
-const NotFoundNote = () => {
+const NotFoundNoteContent = () => {
 	const searchParams = useSearchParams()
 	const code = searchParams.get('code')
 
 	return (
 		<>
 			{code != null && (
-			<div className="container">
-				<span className='note-404 text-center'>404<p className='text-3xl m-0 p-0 uppercase'>{code}</p></span>
-				<Image src="https://logosandtypes.com/wp-content/uploads/2020/08/sas.svg" alt='Brief Url' width={100} height={100} /> <br />
-				<h1>Your shorten url is not found.</h1>
-				<h2>Go to <Link className='link' href={"/"}>Home</Link> and try shortening again.</h2>
-			</div>
+				<div className="container">
+					<span className='note-404 text-center'>404<p className='text-3xl m-0 p-0 uppercase'>{code}</p></span>
+					<Image src="https://logosandtypes.com/wp-content/uploads/2020/08/sas.svg" alt='Brief Url' width={100} height={100} /> <br />
+					<h1>Your shorten url is not found.</h1>
+					<h2>Go to <Link className='link' href={"/"}>Home</Link> and try shortening again.</h2>
+				</div>
 			)}
 		</>
+	)
+}
+
+const NotFoundNote = () => {
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<NotFoundNoteContent />
+		</Suspense>
 	)
 }
 
