cade6864a9971bc3f18cbedda1f8fe61e0fe9a7d
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-01-07 12:07:53 +0530

    Enable dark mode support and enhance styling for improved accessibility

--- src/app/404/page.tsx
+++ src/app/404/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const NotFound = () => {
 	return (
 		<div className="flex flex-col items-center justify-center h-screen">
-			<span className='text-8xl absolute opacity-5 font-bold -rotate-45 -translate-x-1/4 -translate-y-1/2 text-black'>404</span>
+			<span className='text-8xl absolute opacity-5 font-bold -rotate-45 -translate-x-1/4 -translate-y-1/2 text-black dark:text-white'>404</span>
 			<Image src="https://logosandtypes.com/wp-content/uploads/2020/08/sas.svg" alt='Brief Url' width={100} height={100} /> <br />
 			<h1>Your shorten url is not found.</h1>
 			<h2>Go to <Link className='underline cursor-pointer text-blue-500 hover:text-blue-600 active:text-blue-500' href={"/"}>Home</Link> and try shortening again.</h2>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" className="dark">
 			<head>
 				<meta charSet="utf-8" />
 				<link rel="shortcut icon" href="/icon/favicon.ico" type="image/x-icon" />
